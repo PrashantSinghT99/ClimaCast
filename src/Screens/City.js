568e6db96265681a9d8fb10e02d7c3a20e9b8ca8
Prashant Singh <prashantsinghtariyal@gmail.com>
2023-12-09 17:57:41 +0530

    city and icontext component

--- src/Screens/City.js
+++ src/Screens/City.js
@@ -1,19 +1,100 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
-
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+} from 'react-native';
+import IconText from '../Components/IconText';
 const City = () => {
+  const {
+    container,
+    image,
+    city,
+    country,
+    cityText,
+    populationText,
+    populationWrapper,
+    rowLayout,
+    riseSetText,
+    riseSetWrapper,
+  } = styles;
   return (
-    <SafeAreaView>
-      <View>
-        <Text>City</Text>
-      </View>
+    <SafeAreaView style={container}>
+      <ImageBackground source={require('../../assets/city.jpg')} style={image}>
+        <View>
+          <Text style={[city, cityText]}>London</Text>
+          <Text style={[country, cityText]}>UK</Text>
+        </View>
+        <View style={[populationWrapper, rowLayout]}>
+          <IconText
+            iconName={'user'}
+            iconColor={'red'}
+            bodyText={'8000'}
+            bodyTextStyles={populationText}
+          />
+        </View>
+        <View style={[riseSetWrapper, rowLayout]}>
+          <IconText
+            iconName={'sunrise'}
+            iconColor={'white'}
+            bodyText={'10:46:58 AM'}
+            bodyTextStyles={riseSetText}
+          />
+          <IconText
+            iconName={'sunset'}
+            iconColor={'white'}
+            bodyText={'17:28:15 PM'}
+            bodyTextStyles={riseSetText}
+          />
+        </View>
+      </ImageBackground>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    fleX: 1,
+    flex: 1,
+    // marginTop: StatusBar.currentHeight || 0,
+  },
+  image: {
+    flex: 1,
+  },
+  city: {
+    fontSize: 40,
+  },
+  country: {
+    fontSize: 30,
+  },
+  cityText: {
+    justifyContent: 'center',
+    alignSelf: 'center',
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  populationWrapper: {
+    justifyContent: 'center',
+    marginTop: 30,
+  },
+  populationText: {
+    fontSize: 25,
+    marginLeft: 7.5,
+    color: 'red',
+  },
+  riseSetWrapper: {
+    justifyContent: 'space-around',
+    marginTop: 30,
+  },
+  riseSetText: {
+    fontSize: 20,
+    color: 'white',
+  },
+  rowLayout: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
 });
 
